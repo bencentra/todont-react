73ad33b50bc42193ae205e93620758d4ff192c9e
Ben Centra <blcentra@gmail.com>
2019-04-11 19:09:12 -0400

    loading, localStorage

--- src/App.js
+++ src/App.js
@@ -13,22 +13,44 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: true,
       newTodo: '',
       todos: []
     };
   }
 
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        todos: JSON.parse(localStorage.getItem('todos') || '[]'),
+        loading: false,
+      });
+    }, 500);
+  }
+
   updateNewTodoText(e) {
-    this.setState({ newTodo: e.target.value })
+    this.setState({ newTodo: e.target.value });
   }
 
   addTodo() {
     const newTodos = this.state.todos;
-    newTodos.push(this.state.newTodo);
+    newTodos.push({
+      text: this.state.newTodo,
+      key: Date.now(),
+    });
     this.setState({
       todos: newTodos,
       newTodo: '',
+    }, () => {
+      localStorage.setItem('todos', JSON.stringify(this.state.todos));
     });
+  }
+
+  renderTodos() {
+    if (this.state.loading) {
+      return <p>Loading...</p>;
+    } 
+    return <ul>{this.state.todos.map(({ text, key }) => <li key={key}>{text}</li>)}</ul>;
   }
 
   render() {
@@ -46,7 +68,7 @@ class App extends Component {
               <p><Button color="primary" onClick={() => this.addTodo()}>Add Todont</Button></p>
             </Col>
             <Col sm="12" md="8">
-              <ul>{this.state.todos.map((todo, i) => <li key={i}>{todo}</li>)}</ul>
+              {this.renderTodos()}
             </Col>
           </Row>
         </Container>
